2e0fbbfe6438135831edf5f4ddca5fbf8aef2928
melakach <s21_workspace@mail.ru>
2026-05-08 16:08:42 +0500

    Восстановил файл, который передал через Коммит в фича ветку <_<

---
+++ src/main/java/ru/mentee/power/ProgressDemo.java
@@ -0,0 +1,21 @@
+package ru.mentee.power;
+
+// Ветка feature/DVT-3
+public class ProgressDemo {
+
+    public static void main(String[] args) {
+
+        MenteeProgress progress =
+                new MenteeProgress("Nikita", 1, 6);
+
+        System.out.println(progress.summary());
+
+        if (progress.readyForSprint()) {
+            System.out.println("Status: sprint ready");
+        } else {
+            System.out.println("Status: backlog first");
+        }
+
+        System.out.println("Ветка feature/DVT-3");
+    }
+}
